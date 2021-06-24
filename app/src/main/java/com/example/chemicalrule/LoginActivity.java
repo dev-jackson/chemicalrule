63472f95c03f532a45e237eb741d314c408dc6d5
dev-jackson <angeljackson0933@gmail.com>
2021-06-23 22:51:06 -0500

    add corner widgets

--- app/src/main/java/com/example/chemicalrule/LoginActivity.java
+++ app/src/main/java/com/example/chemicalrule/LoginActivity.java
@@ -2,10 +2,17 @@ package com.example.chemicalrule;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,14 +20,33 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-//        Button btn_login = (Button) R.id.btn_login;
+
+
+        Button btn_login = (Button) findViewById(R.id.btn_login);
+
+        //Inputs
+        TextInputLayout text_username = (TextInputLayout) findViewById(R.id.input_username);
+        TextInputLayout text_password = (TextInputLayout) findViewById(R.id.input_password);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("a",text_password.getEditText().getText().toString());
+                Intent enter = new Intent(v.getContext(),MainActivity.class);
+                if(text_password.toString().equals("123")
+                        && text_username.toString().equals("user")
+                ){
+                    enter.putExtra("name",text_username.toString()); //send data
+                    startActivity(enter);
+                }else{
+                    Toast.makeText(v.getContext(),"Credenciales Incorrectas", Toast.LENGTH_LONG).show();
+                }
+                //enter.putExtra("name",text_username.toString()); //send data
+                //Bundle bundle = getIntent().getExtras();
+                // bundle.getString(name_key);
+                //startActivity(enter);
+            }
+        });
     }
 
-    public void enter_activity_main(View view) {
-        Intent enter = new Intent(view.getContext(),MainActivity.class);
-        //enter.putExtra("name","Hello"); send data
-        //Bundle bundle = getIntent().getExtras();
-        // bundle.getString(name_key);
-        startActivity(enter);
-    }
 }
--- app/src/main/java/com/example/chemicalrule/MainActivity.java
+++ app/src/main/java/com/example/chemicalrule/MainActivity.java
@@ -8,9 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.Theme_Splash);
         super.onCreate(savedInstanceState);
-        setTheme(R.style.Theme_ChemicalRule);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
     }
 }
